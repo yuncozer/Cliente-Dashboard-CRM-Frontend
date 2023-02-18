13c87fd577e9ea3905d7eb3a6f99b1faf267bd80
yuncozer <daniel.krdns@gmail.com>
2023-02-18 19:07:12 -0400

    Reset password, login with google, error messages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,48 @@
 import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { 
-        RiBarChart2Line,
-        RiEarthLine,
-        RiArrowRightSLine,
-        RiMenu3Fill,
-        RiCloseFill,
-        RiDownload2Fill,
-        RiDashboard2Fill,
-        RiDoorOpenLine
-        
-    } 
+import {
+    RiBarChart2Line,
+    RiEarthLine,
+    RiArrowRightSLine,
+    RiMenu3Fill,
+    RiCloseFill,
+    RiDownload2Fill,
+    RiDashboard2Fill,
+    RiDoorOpenLine
+
+}
     from "react-icons/ri";
 import html2canvas from "html2canvas";
 import jsPDF from "jspdf";
 import { useAuth } from '../context/authContext';
 
 const Sidebar = () => {
-    
-    
-    const {logout} = useAuth()
-    
-    const handleLogout = () => {
-        logout();
+
+    const [error, setError] = useState()
+    const { logout } = useAuth()
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+        } catch (error) {
+            setError(error.message)
+        }
     }
-    
+
     // Hook Location Page
-  
+
     const { pathname } = useLocation();
     const [layout, page] = pathname.split("/").filter((el) => el !== "");
-    
+
     // Screenshot view page and donwload pdf file
-    
+
     var today = new Date();
     const [showMenu, setShowMenu] = useState(false);
     const [showSubMenu, setShowSubMenu] = useState(false);
     const exportPDF = () => {
-        const input = document.getElementById("Page") 
-        html2canvas(input, {logging: true, letterRendering: 1, useCORS: true}).then(canvas => {
+        const input = document.getElementById("Page")
+        html2canvas(input, { logging: true, letterRendering: 1, useCORS: true }).then(canvas => {
             const imgWidth = 208;
             const imgHeight = canvas.height * imgWidth / canvas.width;
             const imgData = canvas.toDataURL('image/png');
@@ -47,91 +51,91 @@ const Sidebar = () => {
             pdf.save(`${today}dashboard_crm.pdf`)
         })
     }
-    
+
 
     return (
-    <>
-        {/* Menu */}
-        <div id="Prueba" className={`bg-secondary-100 xl:h-[100vh] overflow-y-auto fixed xl:static w-[80%] sm:w-[40%] md:w-[40%] lg:w-[30%] xl:w-auto
+        <>
+            {/* Menu */}
+            <div id="Prueba" className={`bg-secondary-100 xl:h-[100vh] overflow-y-auto fixed xl:static w-[80%] sm:w-[40%] md:w-[40%] lg:w-[30%] xl:w-auto
                         h-full top-0 p-4 flex flex-col justify-between z-50 ${showMenu ? 'left-0' : '-left-full'} transition-all`}>
-        <div>
-            <h1 className='flex items-center text-center text-2xl font-bold text-white mb-10'>
-            <RiDashboard2Fill    className='text-primary mr-2'/>Dashboard CRM {/*<span className='text-primary text-4xl'>.</span> */}
-            </h1>
-            {/* Items */}
-            <ul>
-                {/* CRM Objects */}
-                <li>
-                    <button className='flex items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors w-full'
-                            onClick={() => setShowSubMenu(!showSubMenu)}>
-                        <span className='flex items-center gap-4'>
-                            <RiEarthLine    className='text-primary'/>CRM Objects
-                        </span>
-                        <RiArrowRightSLine className={`mt-1 ${showSubMenu && 'rotate-90'} transition-all`}/>
-                    </button>
-                    {/* Sub-menu Redes Sociales */}
-                    <ul className=  { ` ${showSubMenu ?  'h-fit' : 'h-0'}
-                                        overflow-y-hidden transition-all`
-                                    }>
+                <div>
+                    <h1 className='flex items-center text-center text-2xl font-bold text-white mb-10'>
+                        <RiDashboard2Fill className='text-primary mr-2' />Dashboard CRM {/*<span className='text-primary text-4xl'>.</span> */}
+                    </h1>
+                    {/* Items */}
+                    <ul>
+                        {/* CRM Objects */}
                         <li>
-                            <Link to='/dashboard/contacts' className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3
+                            <button className='flex items-center justify-between py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors w-full'
+                                onClick={() => setShowSubMenu(!showSubMenu)}>
+                                <span className='flex items-center gap-4'>
+                                    <RiEarthLine className='text-primary' />CRM Objects
+                                </span>
+                                <RiArrowRightSLine className={`mt-1 ${showSubMenu && 'rotate-90'} transition-all`} />
+                            </button>
+                            {/* Sub-menu Redes Sociales */}
+                            <ul className={` ${showSubMenu ? 'h-fit' : 'h-0'}
+                                        overflow-y-hidden transition-all`
+                            }>
+                                <li>
+                                    <Link to='/dashboard/contacts' className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3
                                                     before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px]
                                                     before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100 
                                                     hover:text-white hover:before:bg-primary transition-colors'>
-                                Contacts
-                            </Link> 
-                        </li>
-                        <li>
-                            <Link to='/dashboard/companies' className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3
+                                        Contacts
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/dashboard/companies' className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3
                                                     before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px]
                                                     before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100
                                                     hover:text-white hover:before:bg-primary transition-colors'>
-                                Companies
-                            </Link> 
-                        </li>
-                        <li>
-                            <Link to='/dashboard/deals' className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3
+                                        Companies
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/dashboard/deals' className='py-2 px-4 border-l border-gray-500 ml-6 block relative before:w-3 before:h-3
                                                     before:absolute before:bg-gray-500 before:rounded-full before:-left-[6.5px]
                                                     before:top-1/2 before:-translate-y-1/2 before:border-4 before:border-secondary-100
                                                     hover:text-white hover:before:bg-primary transition-colors'>
-                                Deals
-                            </Link> 
+                                        Deals
+                                    </Link>
+                                </li>
+
+                            </ul>
                         </li>
-                        
+                        {/* Account Manager Activity */}
+                        <li>
+                            <Link className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'
+                                to='/dashboard/accountmanagers'>
+                                <RiBarChart2Line className='text-primary' />Account Manager Activity
+                            </Link>
+                        </li>
+                        {/* DownLoad views */}
+                        {page ?
+                            <li>
+                                <Link onClick={() => exportPDF()} className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
+                                    <RiDownload2Fill className='text-primary' />Download View
+                                </Link>
+                            </li> : <div />
+                        }
                     </ul>
-                </li>
-                {/* Account Manager Activity */}
-                <li>
-                    <Link   className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'
-                            to='/dashboard/accountmanagers'>
-                        <RiBarChart2Line    className='text-primary'/>Account Manager Activity
+                </div>
+                <nav>
+                    <Link onClick={() => handleLogout()}
+                        className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'
+                    /*to='/'*/ >
+                        <RiDoorOpenLine className='text-primary' />Exit
                     </Link>
-                </li>
-                {/* DownLoad views */}
-                { page ? 
-                    <li>
-                    <Link onClick={ () => exportPDF() }  className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'>
-                        <RiDownload2Fill    className='text-primary'/>Download View
-                    </Link>
-                    </li> : <div/>  
-                }
-            </ul>
-        </div>
-        <nav>
-            <Link   onClick={ () => handleLogout() }
-                    className='flex items-center gap-4 py-2 px-4 rounded-lg hover:bg-secondary-900 transition-colors'
-                    to='/'>
-                <RiDoorOpenLine    className='text-primary'/>Exit
-            </Link>
-        </nav>
-        </div>
-        {/* Boton Menu Responsive */}
-        <button onClick={ () => setShowMenu(!showMenu)}
+                </nav>
+            </div>
+            {/* Boton Menu Responsive */}
+            <button onClick={() => setShowMenu(!showMenu)}
                 className='xl:hidden fixed bottom-4 right-4 bg-primary text-black p-3 rounded-full z-50'>
-            { !showMenu ? <RiMenu3Fill/> : <RiCloseFill/> }
-        </button>
-    </>
-  )
+                {!showMenu ? <RiMenu3Fill /> : <RiCloseFill />}
+            </button>
+        </>
+    )
 }
 
 export default Sidebar
